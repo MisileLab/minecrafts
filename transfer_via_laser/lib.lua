f73d6f0c9d8c7a22f8caa0bb8b404edc6454dd14
misilelaboratory <misileminecord@gmail.com>
2025-09-05 00:35:40 +0900

    feat(transfer_via_laser): init

---
+++ transfer_via_laser/lib.lua
@@ -0,0 +1,10 @@
+function NumberToBits(num)
+  if num == 0 then return "0" end
+  
+  local bits = {}
+  while num > 0 do
+      table.insert(bits, 1, tostring(num % 2))
+      num = math.floor(num / 2)
+  end
+  return table.concat(bits)
+end
---
+++ transfer_via_laser/main.lua
@@ -0,0 +1,8 @@
+local relayes = {peripheral.find("redstone_relay")}
+local pulse = relayes[1]
+local ed = relayes[2]
+write("> ")
+local stre = read()
+local bytes = {string.byte(stre, 1, stre:len())}
+for i=3,8 do
+end
